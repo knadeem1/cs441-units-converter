d66e2cee0a45da9e477f20a33608b9fdac749d71
knadeem1 <35601623+knadeem1@users.noreply.github.com>
2019-09-03 19:34:02 -0400

    created layout

---
+++ app/src/main/java/edu/binghamton/cs/unit_converter/MainActivity.java
@@ -0,0 +1,23 @@
+package edu.binghamton.cs.unit_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner dropdown = findViewById(R.id.spinner);
+
+        String[] items = new String[]{"Jupiter", "Mars", "Mercury", "Neptune", "Saturn", "Uranus", "Venus"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+//set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+    }
+}
