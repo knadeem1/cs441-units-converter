c1e54ef6f2c5d425729918d8028482f254d1efd1
knadeem1 <35601623+knadeem1@users.noreply.github.com>
2019-09-06 10:41:10 -0400

    configured

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # cs441-units-converter
-Units Converter
+Project 1: Weight Converter
+
+Description: This weight converter will calculate how much you would weigh on other planets. Your weight is different on other planets because gravity is different, although your mass stays the same. We calculate weight by multiplying masss by the gravity on the surface of the planet. This application requires you to enter you mass and pick a planet of your preference to determine how much you would weigh.
+
+ 
+
+
--- app/src/main/java/edu/binghamton/cs/unit_converter/MainActivity.java
+++ app/src/main/java/edu/binghamton/cs/unit_converter/MainActivity.java
@@ -3,21 +3,53 @@ package edu.binghamton.cs.unit_converter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Dictionary;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner dropdown;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner dropdown = findViewById(R.id.spinner);
+        dropdown = findViewById(R.id.spinner);
 
         String[] items = new String[]{"Jupiter", "Mars", "Mercury", "Neptune", "Saturn", "Uranus", "Venus"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-//set the spinners adapter to the previously created one.
         dropdown.setAdapter(adapter);
+    }
+
+    public void convert(View v){
+        EditText text = findViewById(R.id.editText);
+
+        double mass = Double.parseDouble(text.getText().toString());
+        String planet = dropdown.getSelectedItem().toString();
+        double[] planet_gravity = new double[]{2.34, 0.38, 0.38, 1.12, 0.93, .92, .91};
+        String[] items = new String[]{"Jupiter", "Mars", "Mercury", "Neptune", "Saturn", "Uranus", "Venus"};
+        int i = 0;
+        int planet_id = 0;
+        for(String item: items){
+            if(item == planet){
+                planet_id = i;
+            }
+            i= i+1;
+        }
+
+        double weight = (planet_gravity[planet_id] * mass);
+
+        Toast.makeText(MainActivity.this, Double.toString(weight), Toast.LENGTH_LONG).show();
+
     }
 }
